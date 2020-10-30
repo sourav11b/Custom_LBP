f2717301cd0227a5b80b7b8cd6ee693d06f1e858
sourav11b <souravbiswas@gmail.com>
2020-10-30 16:42:40 -0600

    initial

---
+++ src/main/java/com/datastax/custom_lbp/customlbp/test/test.java
@@ -0,0 +1,53 @@
+package com.datastax.custom_lbp.customlbp.test;
+
+import com.datastax.oss.driver.api.core.ConsistencyLevel;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.*;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		try (CqlSession session = CqlSession.builder().addContactPoint(new InetSocketAddress("54.67.24.25", 9042))
+				.addContactPoint(new InetSocketAddress("50.18.97.114", 9042)).withLocalDatacenter("DC1").build()) {
+
+			PreparedStatement prepared = session.prepare("select * from cycling.cyclist_semi_pro  where id = ?");
+			while (true) {
+				try {
+					BoundStatement bound = prepared.bind(new Random().nextInt(10));
+
+					ResultSet rs = session.execute(bound);
+
+					while (rs.getAvailableWithoutFetching() > 0) {
+						Row row = rs.one();
+						System.out.println(row);
+					}
+
+					System.out
+							.println(" coordinator node : " + rs.getExecutionInfo().getCoordinator().getBroadcastAddress().get().getHostName());
+					System.out.println(" coordinator node DC : " + rs.getExecutionInfo().getCoordinator().getDatacenter());
+
+					System.out.println("------------------------------------------------------------");
+
+					Thread.sleep(2000);
+				} catch (Exception e) {
+					System.err.println(" Exception : " + e.getMessage());
+				}
+			}
+
+		}
+
+	}
+
+}
